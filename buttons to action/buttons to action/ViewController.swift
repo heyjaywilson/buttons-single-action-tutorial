1393c1bb79190ebae0b8fec98d42d9f21ad8cbde
maeganwilson_ <maegan@maeganwilson.com>
2019-05-29 21:25:48 -0500

    Adding code and ui

---
+++ buttons to action/buttons to action/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  buttons to action
+//
+//  Created by Maegan Wilson on 5/29/19.
+//  Copyright © 2019 MW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
